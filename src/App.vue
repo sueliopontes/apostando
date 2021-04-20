0685636520a9c25263f4521aa147483fecf11d87
Suélio <suelio.pontes@hotmail.com>
2021-04-20 00:28:01 -0300

    feat:Iniciando o projeto

---
+++ src/App.vue
@@ -0,0 +1,66 @@
+<template>
+  <div id="app">
+    <div class="text-center">  
+      Apostando
+    </div>
+    <div class="text-center" id="numeroInput">  
+      <input type="text" name="campo1"  id="campo1"  size="1" maxlength="2" class="">    
+      <input type="text" name="campo2"  id="campo2"  size="1" maxlength="2" class="">
+      <input type="text" name="campo3"  id="campo3"  size="1" maxlength="2" class="">    
+      <input type="text" name="campo4"  id="campo4"  size="1" maxlength="2" class="">
+      <input type="text" name="campo5"  id="campo5"  size="1" maxlength="2" class="">
+
+      <br>
+
+      <input type="text" name="campo6"  id="campo6"  size="1" maxlength="2" class="">      
+      <input type="text" name="campo7"  id="campo7"  size="1" maxlength="2" class="">    
+      <input type="text" name="campo8"  id="campo8"  size="1" maxlength="2" class="">
+      <input type="text" name="campo9"  id="campo9"  size="1" maxlength="2" class="">    
+      <input type="text" name="campo10" id="campo10" size="1" maxlength="2" class=""> 
+
+      <br>
+
+      <input type="text" name="campo11" id="campo11" size="1" maxlength="2" class="">    
+      <input type="text" name="campo12" id="campo12" size="1" maxlength="2" class="">
+      <input type="text" name="campo13" id="campo13" size="1" maxlength="2" class="">    
+      <input type="text" name="campo14" id="campo14" size="1" maxlength="2" class="">      
+      <input type="text" name="campo15" id="campo15" size="1" maxlength="2" class=""> 
+
+      <br>
+
+      <input type="text" name="campo16" id="campo16" size="1" maxlength="2" class="">
+      <input type="text" name="campo17" id="campo17" size="1" maxlength="2" class="">    
+      <input type="text" name="campo18" id="campo18" size="1" maxlength="2" class="">
+      <input type="text" name="campo19" id="campo19" size="1" maxlength="2" class="">   
+      <input type="text" name="campo20" id="campo20" size="1" maxlength="2" class=""> 
+
+      <br>
+
+      <input type="text" name="campo21" id="campo21" size="1" maxlength="2" class="">    
+      <input type="text" name="campo22" id="campo22" size="1" maxlength="2" class="">
+      <input type="text" name="campo23" id="campo23" size="1" maxlength="2" class="">    
+      <input type="text" name="campo24" id="campo24" size="1" maxlength="2" class="">
+      <input type="text" name="campo25" id="campo25" size="1" maxlength="2" class="">     
+      
+    </div>
+    
+  </div>
+</template>
+
+<script>
+import "bootstrap/dist/css/bootstrap.css"
+
+export default {
+  name: 'App'
+  
+}
+</script>
+
+<style>
+
+#numeroInput{
+  margin:10px,10px,10px,10px;
+  
+}
+
+</style>
